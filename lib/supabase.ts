f4f92b0eb00c4792ae17f5121d93174527a973b4
steveengreen436-art <steveen.green436@gmail.com>
2026-07-11 00:06:51 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables! Check your Vercel settings.");
-}
+// Replace these strings with your ACTUAL keys from the Supabase API page
+const supabaseUrl = "sb_publishable_u0djiaYiyJTeMmIWVjzd2w_XHTPuGyo";
+const supabaseAnonKey = "b_secret_0oT8f4nFoLNG6ktzMvMelw_qq-7cBHx";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
